ee497f8ecede25c04b84a11a30e6dd33f51ebb44
Jan Monschke <jan.monschke@gmail.com>
2017-12-09 14:03:53 +0100

    add skeleton

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './normalize.css';
+import './skeleton.css';
 
 const Header = () => (
   <div
