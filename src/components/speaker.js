a93c075876dcff7ea00bac064a511d9e66f240ca
Tom Jenkinson <tom.jenkinson@soundcloud.com>
2018-03-06 22:57:12 +0100

    make speaker semantic

--- src/components/speaker.js
+++ src/components/speaker.js
@@ -3,24 +3,25 @@ import { withPrefix } from 'gatsby-link';
 import './speaker.css';
 
 export default function({
-  name,
+  givenName,
+  familyName,
   imagePath,
   children
 }) {
   const text = (
     <div className={imagePath ? 'eight columns' : 'twelve columns'}>
-      <h2>{ name }</h2>
+      <h2><span itemProp="givenName">{ givenName }</span> <span itemProp="familyName">{ familyName }</span></h2>
       { children }
     </div>
   );
   const image = imagePath ? (
     <div className='four columns'>
-      <img src={withPrefix(imagePath)} alt={name} className='img-responsive img-rounded' />
+      <img src={withPrefix(imagePath)} alt={name} className='img-responsive img-rounded' itemProp="image" />
     </div>
   ) : null;
 
   return (
-    <div className='row speaker'>
+    <div className='row speaker' itemScope itemType="http://schema.org/Person">
       { text }
       { image }
     </div>
