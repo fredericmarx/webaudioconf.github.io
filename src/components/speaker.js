aab38f211dc83e02f5dbfd853018e6c782d61ede
Tom Jenkinson <tom.jenkinson@soundcloud.com>
2018-03-14 22:54:36 +0100

    put speaker image above text on small screen

--- src/components/speaker.js
+++ src/components/speaker.js
@@ -12,21 +12,23 @@ export default function({
 }) {
   const keynoteBadge = keynote ? <Badge style={{ marginLeft: '10px' }}>Keynote</Badge> : null;
   const text = (
-    <div className={imagePath ? 'eight columns' : 'twelve columns'}>
-      <h2><span itemProp="givenName">{ givenName }</span> <span itemProp="familyName">{ familyName }</span>{ keynoteBadge }</h2>
+    <div className='speaker__content'>
       { children }
     </div>
   );
   const image = imagePath ? (
-    <div className='four columns'>
+    <div className='speaker__photo'>
       <img src={withPrefix(imagePath)} alt={`photo of ${givenName} ${familyName}`} className='img-responsive img-rounded' itemProp="image" />
     </div>
   ) : null;
 
   return (
     <div className='row speaker' itemScope itemType="http://schema.org/Person">
-      { text }
-      { image }
+      <h2><span itemProp="givenName">{ givenName }</span> <span itemProp="familyName">{ familyName }</span>{ keynoteBadge }</h2>
+      <div className='speaker__flex'>
+        { text }
+        { image }
+      </div>
     </div>
   );
 }
