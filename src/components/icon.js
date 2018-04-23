19aa8f7be0f91ee58b8c83f2f8604e89d993ad47
Tom Jenkinson <tom.jenkinson@soundcloud.com>
2018-04-23 19:38:51 +0200

    add GitHub component

--- src/components/icon.js
+++ src/components/icon.js
@@ -7,8 +7,12 @@ export default function ({ name, color }) {
   if (!color) {
     switch(name) {
       case 'twitter':
-      color = '#1ea1f2';
+        color = '#1ea1f2';
+        break;
+      default:
+        color = '#000000';
+        break;
     }
   }
-  return <i className={`fa fa-${name}`} style={{ color }}></i>;
+  return <i className={`fa fa-${name}`} style={{ color, marginRight: '0.15em' }}></i>;
 }
