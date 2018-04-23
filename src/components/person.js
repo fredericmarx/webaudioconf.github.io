1808c1c75003c7bc4dc18873ee3549f4a9a899a9
Tom Jenkinson <tom.jenkinson@soundcloud.com>
2018-04-23 19:27:51 +0200

    rename 'speaker' component to 'person'

--- src/components/person.js
+++ src/components/person.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { withPrefix } from 'gatsby-link';
 import Badge from './badge';
-import './speaker.css';
+import './person.css';
 
 export default function ({
   givenName,
   familyName,
   imagePath,
-  keynote = false,
+  role,
   children
 }) {
-  const keynoteBadge = keynote ? <Badge style={{ marginLeft: '10px' }}>Keynote</Badge> : null;
+  const keynoteBadge = role ? <Badge style={{ marginLeft: '10px' }}>{ role }</Badge> : null;
   const text = (
-    <div className='speaker__content'>
+    <div className='person__content'>
       {children}
     </div>
   );
   const image = imagePath ? (
-    <div className='speaker__photo'>
+    <div className='person__photo'>
       <img src={withPrefix(imagePath)} alt={`photo of ${givenName} ${familyName}`} className='img-responsive img-rounded' itemProp="image" />
     </div>
   ) : null;
 
   return (
-    <div className='row speaker' itemScope itemType="http://schema.org/Person">
-      <div className='speaker__textContent'>
+    <div className='row person' itemScope itemType="http://schema.org/Person">
+      <div className='person__textContent'>
         <h2>
           <span itemProp="givenName">{givenName}</span> <span itemProp="familyName">{familyName}</span>{keynoteBadge}
         </h2>
