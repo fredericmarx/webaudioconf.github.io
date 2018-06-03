55ec8bbb85fe256a72d955896c9da73c753a7b12
Tom Jenkinson <tjcode@outlook.com>
2018-06-03 21:04:16 +0200

    add space

--- src/helpers/break-parts.js
+++ src/helpers/break-parts.js
@@ -3,6 +3,6 @@ import React from 'react';
 export function breakParts(parts, seperator) {
   const l = parts.length;
   return parts.map((a, i) => {
-    return <span style={{ display: 'inline-block'}}>{`${a}${i < l - 1 ? seperator : ''}`}</span>;
+    return <span style={{ display: 'inline-block' }}>{`${a}${i < l - 1 ? seperator : ''}`}</span>;
   });
 }
