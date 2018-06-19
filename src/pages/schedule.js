2cc91a9b6d8c5287ca1781a34f3881e24a668847
Jan Monschke <jan.monschke@gmail.com>
2018-06-19 23:58:18 +0200

    calculate times from UTC and highlight current entry

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -6,7 +6,8 @@ const schedule = {
     title: 'Wednesday, Sept 19th',
     entries: [
       {
-        time: '08:00 - 09:00',
+        startTime: Date.UTC(2018, 8, 19, 8, 0),
+        endTime: Date.UTC(2018, 9, 19, 9, 0),
         title: 'Registration',
         description: 'Get your conference badge and mingle with other attendees',
         location: {
@@ -15,28 +16,34 @@ const schedule = {
         }
       },
       {
-        time: '09:00 - 10:00',
+        startTime: Date.UTC(2018, 8, 19, 9, 0),
+        endTime: Date.UTC(2018, 8, 19, 10, 0),
         title: 'Keynote',
         description: 'The opening keynote for WAC 2018'
       },
       {
-        time: '10:00 - 10:30',
+        startTime: Date.UTC(2018, 8, 19, 10, 0),
+        endTime: Date.UTC(2018, 8, 19, 10, 30),
         title: 'Coffee & Tea break'
       },
       {
-        time: '10:30 - 12:30',
+        startTime: Date.UTC(2018, 8, 19, 10, 30),
+        endTime: Date.UTC(2018, 8, 19, 12, 30),
         title: 'Presentations'
       },
       {
-        time: '12:30 - 14:00',
+        startTime: Date.UTC(2018, 8, 19, 12, 30),
+        endTime: Date.UTC(2018, 8, 19, 14, 0),
         title: 'Lunch break'
       },
       {
-        time: '14:00 - 16:00',
+        startTime: Date.UTC(2018, 8, 19, 14, 0),
+        endTime: Date.UTC(2018, 8, 19, 16, 0),
         title: 'Presentations'
       },
       {
-        time: '16:00 - 18:00',
+        startTime: Date.UTC(2018, 8, 19, 16, 0),
+        endTime: Date.UTC(2018, 8, 19, 18, 0),
         title: 'Demos, Posters and Installations',
         description: `
           Check out a variety of demos, posters and installations in various locations on the campus.
@@ -44,7 +51,8 @@ const schedule = {
         `
       },
       {
-        time: '20:00 - 23:00',
+        startTime: Date.UTC(2018, 8, 19, 20, 0),
+        endTime: Date.UTC(2018, 8, 19, 23, 0),
         title: 'Concert',
         location: {
           name: 'SoundCloud',
@@ -57,7 +65,8 @@ const schedule = {
     title: 'Thursday, Sept 20th',
     entries: [
       {
-        time: '08:00 - 09:00',
+        startTime: Date.UTC(2018, 8, 20, 8, 0),
+        endTime: Date.UTC(2018, 8, 20, 9, 0),
         title: 'Registration',
         description: 'Get your conference badge and mingle with other attendees',
         location: {
@@ -66,28 +75,34 @@ const schedule = {
         }
       },
       {
-        time: '09:00 - 10:00',
+        startTime: Date.UTC(2018, 8, 20, 9, 0),
+        endTime: Date.UTC(2018, 8, 20, 10, 0),
         title: 'Keynote',
         description: 'The second keynote of WAC 2018'
       },
       {
-        time: '10:00 - 10:30',
+        startTime: Date.UTC(2018, 8, 20, 10, 0),
+        endTime: Date.UTC(2018, 8, 20, 10, 30),
         title: 'Coffee & Tea break'
       },
       {
-        time: '10:30 - 12:30',
+        startTime: Date.UTC(2018, 8, 20, 10, 30),
+        endTime: Date.UTC(2018, 8, 20, 12, 30),
         title: 'Presentations'
       },
       {
-        time: '12:30 - 14:00',
+        startTime: Date.UTC(2018, 8, 20, 12, 30),
+        endTime: Date.UTC(2018, 8, 20, 14, 0),
         title: 'Lunch break'
       },
       {
-        time: '14:00 - 16:00',
+        startTime: Date.UTC(2018, 8, 20, 14, 0),
+        endTime: Date.UTC(2018, 8, 20, 16, 0),
         title: 'Presentations'
       },
       {
-        time: '16:00 - 18:00',
+        startTime: Date.UTC(2018, 8, 20, 16, 0),
+        endTime: Date.UTC(2018, 8, 20, 18, 0),
         title: 'Demos, Posters and Installations',
         description: `
           Check out a variety of demos, posters and installations in various locations on the campus.
@@ -95,7 +110,8 @@ const schedule = {
         `
       },
       {
-        time: '20:00 - 23:00',
+        startTime: Date.UTC(2018, 8, 20, 20, 0),
+        endTime: Date.UTC(2018, 8, 20, 23, 0),
         title: 'Dinner',
         description: 'We are preparing a very special audio-enhanced dinner together with Ben Houge and Brlo',
         location: {
@@ -109,7 +125,8 @@ const schedule = {
     title: 'Friday, Sept 21st',
     entries: [
       {
-        time: '08:00 - 09:00',
+        startTime: Date.UTC(2018, 8, 21, 8, 0),
+        endTime: Date.UTC(2018, 8, 21, 9, 0),
         title: 'Registration',
         description: 'Get your conference badge and mingle with other attendees',
         location: {
@@ -118,63 +135,119 @@ const schedule = {
         }
       },
       {
-        time: '09:00 - 12:30',
+        startTime: Date.UTC(2018, 8, 21, 9, 0),
+        endTime: Date.UTC(2018, 8, 21, 12, 30),
         title: 'Workshops'
       },
       {
-        time: '12:30 - 14:00',
+        startTime: Date.UTC(2018, 8, 21, 12, 30),
+        endTime: Date.UTC(2018, 8, 21, 14, 0),
         title: 'Lunch break'
       },
       {
-        time: '14:00 - 18:00',
+        startTime: Date.UTC(2018, 8, 21, 14, 0),
+        endTime: Date.UTC(2018, 8, 21, 18, 0),
         title: 'Workshops'
       },
       {
-        time: '18:00 - 18:15',
+        startTime: Date.UTC(2018, 8, 21, 18, 0),
+        endTime: Date.UTC(2018, 8, 21, 18, 15),
         title: 'End of the conference'
       }
     ]
   }
 }
 
-function renderDay(day) {
-  return (
-    <div className="schedule__day">
-      <h2 className="schedule__headlineDay">{day.title}</h2>
-      <div className="schedule__timeTable">
-        {day.entries.map((entry) => (
-          <div key={`${day.title}${entry.time}`} className="schedule__entry">
-            <div className="schedule__entryTime">
-              {entry.time}
-            </div>
-            <div className="schedule__entryInfo">
-              <div className="schedule__entryTitle">
-                {entry.title}
-                {entry.location && (
-                  <span className="schedule__entryLocation">
-                    (<a href={entry.location.link} target="_blank">{entry.location.name}</a>)
-                  </span>
-                )}
-              </div>
-              {entry.description && (
-                <div className="schedule__entryDescription">{entry.description}</div>
-              )}
-            </div>
-          </div>
-        ))}
+export default class Schedule extends React.Component {
+  state = {}
 
+  componentDidMount() {
+    this.updateHandle = setInterval(this.updateTime, 1000 * 60)
+    this.updateTime();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.updateHandle);
+  }
+
+  updateTime = () => {
+    this.setState({
+      currentTime: nowInUTC()
+    });
+  }
+
+  renderDay = (day) => {
+    const currentTime = this.state.currentTime || nowInUTC();
+    return (
+      <div className="schedule__day">
+        <h2 className="schedule__headlineDay">{day.title}</h2>
+        <div className="schedule__timeTable">
+          {day.entries.map((entry) => {
+            const isCurrent = currentTime >= entry.startTime && currentTime <= entry.endTime;
+            const entryStart = new Date(entry.startTime);
+            const entryEnd = new Date(entry.endTime);
+            const time = "" +
+              zeroPad(entryStart.getUTCHours()) + ":" + zeroPad(entryStart.getUTCMinutes()) +
+              " - " +
+              zeroPad(entryEnd.getUTCHours()) +
+              ":" + zeroPad(entryEnd.getUTCMinutes())
+              ;
+
+            return (
+              <div
+                key={`${day.title}${time}`}
+                id={`${isCurrent ? 'current' : ''}`}
+                className={`schedule__entry ${isCurrent ? 'm-current' : ''}`}
+              >
+                <div className="schedule__entryTime">
+                  {time}
+                </div>
+                <div className="schedule__entryInfo">
+                  <div className="schedule__entryTitle">
+                    {entry.title}
+                    {entry.location && (
+                      <span className="schedule__entryLocation">
+                        (<a href={entry.location.link} target="_blank">{entry.location.name}</a>)
+                    </span>
+                    )}
+                  </div>
+                  {entry.description && (
+                    <div className="schedule__entryDescription">{entry.description}</div>
+                  )}
+                </div>
+              </div>
+            )
+          })}
+
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  render() {
+    return (
+      <div className="schedule">
+        <h1>Schedule</h1>
+        {this.renderDay(schedule.day1)}
+        {this.renderDay(schedule.day2)}
+        {this.renderDay(schedule.day3)}
+      </div>
+    );
+  }
 }
 
-const Schedule = () => (
-  <div className="schedule">
-    <h1>Schedule</h1>
-    {renderDay(schedule.day1)}
-    {renderDay(schedule.day2)}
-    {renderDay(schedule.day3)}
-  </div>
-)
+function zeroPad(number) {
+  return number <= 9 ? `0${number}` : `${number}`;
+}
 
-export default Schedule;
+function nowInUTC() {
+  const now = new Date();
+
+  return Date.UTC(
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate(),
+    now.getUTCHours(),
+    now.getUTCMinutes()
+  );
+}
