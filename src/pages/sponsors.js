f8cbb5a4bdb112adbb65a504c640a1a8137907a8
janmonschke <jan.monschke@gmail.com>
2018-06-29 18:07:05 +0200

    add amped-studio

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import Announcement from '../components/announcement';
 import Link from 'gatsby-link';
-import ExternalLink from '../components/link';
+import Sponsor from '../components/sponsor';
 import { withPrefix } from 'gatsby-link';
 import './sponsors.css';
 import abletonLogo from '../sponsors/ableton.svg';
+import ampedStudioLogo from '../sponsors/ampedstudio.png';
+
+const sponsors = {
+  silver: {
+    link: 'https://ampedstudio.com',
+    name: 'Amped Studio',
+    image: {
+      src: ampedStudioLogo,
+      alt: 'Amped Studio logo'
+    }
+  },
+  diversity: {
+    link: "https://ableton.com",
+    image: {
+      src: abletonLogo,
+      alt: 'Ableton logo',
+      className: 'sponsors__ableton'
+    }
+  }
+};
 
 export default function Sponsors() {
   return (
     <div className="sponsors">
       <h1>Sponsors</h1>
       <Announcement>
-        We are still looking for sponsors! If you are interested in sponsoring, have a look at our <Link to={withPrefix('/sponsoring.pdf')}>sponsoring information</Link> and contact us at <Link to="mailto:chairs@webaudioconf.com">chairs@webaudioconf.com</Link>.
+        If you are interested in sponsoring, have a look at our <Link to={withPrefix('/sponsoring.pdf')}>sponsoring information</Link> and contact us at <Link to="mailto:chairs@webaudioconf.com">chairs@webaudioconf.com</Link>.
       </Announcement>
-      <h3>Diversity sponsor</h3>
-      <ExternalLink href="https://ableton.com">
-        <img className="sponsors__ableton" src={abletonLogo} alt="Ableton logo" />
-      </ExternalLink>
+
+      <h3>Silver sponsor</h3>
+      <div className="sponsors__list">
+        <Sponsor {...sponsors.silver} type="silver" />
+      </div>
+
+      <h4>Diversity sponsor</h4>
+      <Sponsor {...sponsors.diversity} type="diversity" />
+
     </div>
   )
 }
