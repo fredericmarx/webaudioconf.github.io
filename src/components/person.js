58842a547408b51f47ed37cd0a71c1d92d7784e0
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-07-13 20:23:44 +0200

    remove keynotes badge

--- src/components/person.js
+++ src/components/person.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withPrefix } from 'gatsby-link';
-import Badge from './badge';
 import { breakParts } from '../helpers/break-parts';
 import './person.css';
 
@@ -9,10 +8,8 @@ export default function ({
   familyName,
   imagePath,
   role,
-  tag,
   children
 }) {
-  const badge = tag ? <Badge style={{ marginLeft: '10px' }}>{ tag }</Badge> : null;
   const roleHeading = role ? <h4>{ breakParts(role.split('/'), '/') }</h4> : null;
   const text = (
     <div className='person__content'>
@@ -29,7 +26,7 @@ export default function ({
     <div className='row person' itemScope itemType="http://schema.org/Person">
       <div className='person__textContent'>
         <h2 style={{ marginBottom: role ? 0 : undefined }}>
-          <span itemProp="givenName">{givenName}</span> <span itemProp="familyName">{familyName}</span>{badge}
+          <span itemProp="givenName">{givenName}</span> <span itemProp="familyName">{familyName}</span>
         </h2>
         {roleHeading}
         {text}
--- src/pages/keynotes.js
+++ src/pages/keynotes.js
@@ -3,8 +3,6 @@ import Person from '../components/person';
 import Twitter from '../components/twitter';
 import Website from '../components/website';
 
-const KEYNOTE = "keynote";
-
 const Keynotes = () => (
   <div className="keynotes">
     <h1>Keynotes</h1>
@@ -12,7 +10,6 @@ const Keynotes = () => (
       givenName='Ruth'
       familyName='John'
       imagePath='keynotes/ruth.jpg'
-      tag={KEYNOTE}
     >
       <p>Ruth is a web technologist and Google Developer Expert. She likes to educate people about new web technologies and inspire them to try them, coming up with exciting and engaging ways to use them. She’s a founding member of <span style={{ whiteSpace: 'nowrap' }}>{'{'} Live: JS {'}'}</span>, a collective of audio and visual artists that solely rely on JavaScript to create their performances. Her favourite things include hacking with hardware, evangelising about Web Audio and MIDI and taking her award nominated, hand coded, audio/visual software to the streets.</p>
       <p><Twitter account='Rumyra' /></p>
@@ -21,7 +18,6 @@ const Keynotes = () => (
       givenName='Chris'
       familyName='Rogers'
       imagePath='keynotes/chris.jpg'
-      tag={KEYNOTE}
     >
       <p>Chris Rogers is a software architect, specializing in audio production tools and music applications. Previously he worked at Google, where he developed the Web Audio API and other advanced web platform technologies. At Apple he was a principal designer of the Audio Units plugin architecture, and developed many of the ones shipping on Mac OS X and iOS, including the AUMatrixReverb, and the AUTimePitch time-stretching algorithm. He's also worked for Macromedia, Motorola, Wolfram Research, and IRCAM where he worked on SVP and developed the original AudioSculpt application.</p>
     </Person>
@@ -29,7 +25,6 @@ const Keynotes = () => (
       givenName='Paul'
       familyName='Adenot'
       imagePath='keynotes/paul.jpg'
-      tag={KEYNOTE}
     >
       <p>Paul Adenot is a platform engineer at Mozilla, working on the Firefox web browser. He is involved in Firefox Web Audio implementation, as well as the system audio code on all platforms, and occasionally helps on WebRTC. He also co-edits the Web Audio API specification at the W3C, and dabbles in music production.</p>
       <Website url='https://paul.cx/' />
