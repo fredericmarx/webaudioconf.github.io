d117a0b4568fcab30509116fe9e7e87f5d991c6d
janmonschke <jan.monschke@gmail.com>
2018-09-07 19:56:11 +0200

    add novation's logo

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import Sponsor from '../components/sponsor';
+import React from "react";
+import Sponsor from "../components/sponsor";
 
-import './sponsors.css';
-import abletonLogo from '../sponsors/ableton.svg';
-import ampedStudioLogo from '../sponsors/ampedstudio.png';
-import mozillaLogo from '../sponsors/mozilla.svg';
+import "./sponsors.css";
+import abletonLogo from "../sponsors/ableton.svg";
+import ampedStudioLogo from "../sponsors/ampedstudio.png";
+import mozillaLogo from "../sponsors/mozilla.svg";
+import novationLogo from "../sponsors/novation.svg";
 
 const sponsors = {
   banquet: {
-    link: 'https://novationmusic.com',
-    name: 'Novation',
+    link: "https://novationmusic.com",
     image: {
-      src: ampedStudioLogo,
-      alt: 'Novation logo'
+      src: novationLogo,
+      alt: "Novation logo",
+      className: "sponsors__novation"
     }
   },
   silver: {
-    link: 'https://ampedstudio.com',
-    name: 'Amped Studio',
+    link: "https://ampedstudio.com",
     image: {
       src: ampedStudioLogo,
-      alt: 'Amped Studio logo'
+      alt: "Amped Studio logo"
     }
   },
   diversity: {
     link: "https://ableton.com",
     image: {
       src: abletonLogo,
-      alt: 'Ableton logo',
-      className: 'sponsors__ableton'
+      alt: "Ableton logo",
+      className: "sponsors__ableton"
     }
   },
   bronze: {
     link: "https://mozilla.com",
     image: {
       src: mozillaLogo,
-      alt: 'Mozilla logo',
-      className: 'sponsors__mozilla'
+      alt: "Mozilla logo",
+      className: "sponsors__mozilla"
     }
   }
 };
@@ -63,7 +63,6 @@ export default function Sponsors() {
 
       <h3>Bronze sponsor</h3>
       <Sponsor {...sponsors.bronze} type="bronze" />
-
     </div>
-  )
+  );
 }
