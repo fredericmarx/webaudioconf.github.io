4604e9dd7b6ff16bc3b49b9f4ee137a5075d695a
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-09-12 06:43:37 +0200

    extract author list in a separate component

--- src/components/demo-or-poster.js
+++ src/components/demo-or-poster.js
@@ -1,23 +1,13 @@
 import { withPrefix } from 'gatsby-link';
 import React from 'react';
 import Abstract from './abstract';
-import Link from './link';
+import Authors from './authors';
 
 export default function DemoOrPoster({ pathContext }) {
-  const authors = pathContext.authors
-    .map(({ link, name }) => link ? <Link href={link} key={name}>{name}</Link> : name)
-    .reduce((accumulator, author, index, array) => {
-      if (index === 0) {
-        return author;
-      }
-
-      return [ ...accumulator, ...((index === array.length - 1) ? [ ' and ', author ] : [ ', ', author ]) ];
-    }, '');
-
   return (
     <div>
       <h1>{ pathContext.title }</h1>
-      <h2>{ authors }</h2>
+      <Authors authors={pathContext.authors}></Authors>
       <Abstract abstract={pathContext.abstract}></Abstract>
     </div>
   );
--- src/components/presentation.js
+++ src/components/presentation.js
@@ -1,23 +1,13 @@
 import { withPrefix } from 'gatsby-link';
 import React from 'react';
 import Abstract from './abstract';
-import Link from './link';
+import Authors from './authors';
 
 export default function Presentation({ pathContext }) {
-  const authors = pathContext.authors
-    .map(({ link, name }) => link ? <Link href={link} key={name}>{name}</Link> : name)
-    .reduce((accumulator, author, index, array) => {
-      if (index === 0) {
-        return author;
-      }
-
-      return [ ...accumulator, ...((index === array.length - 1) ? [ ' and ', author ] : [ ', ', author ]) ];
-    }, '');
-
   return (
     <div>
       <h1>{ pathContext.title }</h1>
-      <h2>{ authors }</h2>
+      <Authors authors={pathContext.authors}></Authors>
       <Abstract abstract={pathContext.abstract}></Abstract>
       { pathContext.type === 'paper' && (
         <a download={`${ pathContext.slug }.pdf`} href={withPrefix(`papers/${ pathContext.slug }.pdf`)}>Paper as PDF</a>
