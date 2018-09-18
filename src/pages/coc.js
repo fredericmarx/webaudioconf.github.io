c6a3f935f5374a6b5591948f5d6ba22084d37a85
Frederic Marx <marx.frederic@gmail.com>
2018-09-18 08:31:49 +0200

    Clarify language in Code of Conduct

--- src/pages/coc.js
+++ src/pages/coc.js
@@ -8,12 +8,12 @@ const CodeOfConduct = () => (
     <p>All attendees, speakers, sponsors, and volunteers at the WAC are required to agree with the following code of conduct. The WAC code of conduct is based on the <a href="http://berlincodeofconduct.org">Berlin Code of Conduct</a>.</p>
 
     <h3>Purpose</h3>
-    <p>A primary goal of all the conferences and user groups that refer to this Code of Conduct is to be inclusive to the largest number of contributors, with the most varied and diverse backgrounds possible. As such, we are committed to providing a friendly, safe and welcoming environment for all, regardless of gender, sexual orientation, ability, ethnicity, socioeconomic status and religion (or lack thereof).</p>
+    <p>A primary goal of this conference is to be inclusive to the largest number of contributors, with the most varied and diverse backgrounds possible. As such, we are committed to providing a friendly, safe and welcoming environment for all, regardless of gender, sexual orientation, ability, ethnicity, socioeconomic status and religion (or lack thereof).</p>
     <p>This Code of Conduct outlines our expectations for all those who participate in our community, as well as the consequences for unacceptable behavior.</p>
     <p>We invite all those who participate in our events to help us create safe and positive experiences for everyone.</p>
 
-    <h3>Open [Source/Culture/Tech] Citizenship</h3>
-    <p>A supplemental goal of this Code of Conduct is to increase open [source/culture/tech] citizenship by encouraging participants to recognize and strengthen the relationships between our actions and their effects on our community.</p>
+    <h3>Open Citizenship</h3>
+    <p>A supplemental goal of this Code of Conduct is to increase open citizenship by encouraging participants to recognize and strengthen the relationships between our actions and their effects on our community.</p>
     <p>Communities mirror the societies in which they exist and positive action is essential to counteract the many forms of inequality and abuses of power that exist in society.</p>
     <p>If you see someone who is making an extra effort to ensure our community is welcoming, friendly, and encourages all participants to contribute to the fullest extent, we want to know.</p>
 
@@ -35,7 +35,7 @@ const CodeOfConduct = () => (
     <p>If a community member engages in unacceptable behavior, the community organizers may take any action they deem appropriate, up to and including a temporary ban or permanent expulsion from the community without warning (and without refund in the case of a paid event).</p>
 
     <h3>If You Witness or Are Subject to Unacceptable Behavior</h3>
-    <p>If you are subject to or witness unacceptable behavior, or have any other concerns, please notify a community organizer as soon as possible. You can find the contact details of the Code of Conduct team at the bottom of this document. Additionally, community organizers are available to help community members engage with local law enforcement or to otherwise help those experiencing unacceptable behavior feel safe. In the context of in-person events, organizers will also provide escorts as desired by the person experiencing distress.</p>
+    <p>If you are subject to or witness unacceptable behavior, or have any other concerns, please notify a community organizer as soon as possible. You can find the contact details of the Code of Conduct team at the bottom of this document. Additionally, community organizers are available to help community members engage with local law enforcement or to otherwise help those experiencing unacceptable behavior feel safe. Organizers will also provide escorts as desired by the person experiencing distress.</p>
 
     <h3>Addressing Grievances</h3>
     <p>If you feel you have been falsely or unfairly accused of violating this Code of Conduct, you should notify one of the event organizers with a concise description of your grievance. Your grievance will be handled in accordance with our existing governing policies.</p>
@@ -47,11 +47,10 @@ const CodeOfConduct = () => (
     <p>Berlin Code of Conduct is distributed under a Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) license. It is based on the pdx.rb code of conduct.</p>
 
     <h2 className="codeOfConduct__reporting">Reporting Violations</h2>
-    <h4>Anonymous Report</h4>
-    <p>We will post a form here for submitting anonymous reports prior to the conference. We can’t follow up an anonymous report with you directly, but we will fully investigate it and take whatever action is necessary to prevent a recurrence.</p>
     <h4>Personal Report</h4>
     <p>You can make a personal report by:</p>
     <ul>
+      <li>Approaching a member of staff wearing a conference t-shirt in person</li>
       <li>Emailing us at: <a href="mailto:chairs@webaudioconf.com">chairs@webaudioconf.com</a></li>
       <li>Calling us at: +49 157 789 021 18</li>
     </ul>
